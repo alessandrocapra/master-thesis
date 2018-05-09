ae4cf0b62ba2beed78066aa0e003639dffefaa0f
Alessandro Capra <alessandro.cpr@gmail.com>
2018-05-09 12:01:41 +0200

    first commit

---
+++ controllers/UserController.js
@@ -0,0 +1,54 @@
+module.exports = {
+  async createUser(req, res) {
+    /*try {
+      const player = await Player.create(req.body)
+      res.send(player.toJSON())
+    } catch(err) {
+      res.status(400).send({
+        error: 'Name already in use!'
+      })
+      // user already existing?
+    }*/
+  },
+  async updateUser(req, res) {
+    /*try {
+      await Player.update({
+        skipTopics: req.body.skipTopics,
+        skipQuestions: req.body.skipQuestions
+      }, {
+        where: {id: req.body.id}
+      }).then(result => res.send(result))
+    } catch(err) {
+      res.status(400).send({
+        error: err.message
+      })
+      // user already existing?
+    }*/
+  },
+  async getUsers(req,res) {
+    /*try {
+      const players = await Player.findAll({
+        order: [
+          ['name', 'ASC'],
+          ['lastname', 'ASC'],
+        ],})
+      res.send(players)
+    } catch(err) {
+      res.status(400).send({
+        error: 'Couldn\'t get the players!'
+      })
+    }*/
+  },
+  async getUser(req,res) {
+    /*try {
+      const player = await Player.findOne({ where: {id: req.params.id} })
+      res.send(player)
+    } catch(err) {
+      res.status(400).send({
+        error: 'Couldn\'t get the player details!'
+      })
+    }*/
+  },
+  async deleteUser(req,res) {
+  }
+};
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+// Importing the controllers containing the desired functions to communicate with the DB
+const userController = require('./controllers/UserController');
+
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.render('index', { title: 'Express' });
+});
+
+
+/*
+
+  The project will need several APIs covering different aspects:
+
+  * USER
+    - signing up
+    - logging in
+    - saving game data to the user profile
+    - retrieve such data
+
+  * SENSING DATA
+    - get the pressure sensor data received by Arduino and send it to PhaserIO to control the game
+
+ */
+
+/*  "/api/users"
+ *    GET: get list of all existing users
+ *    POST: create a new user
+ */
+
+router.get('/api/users', userController.getUsers);
+router.post('/api/users', userController.createUser);
+
+/*  "/api/users/:id"
+ *    GET: find user by id
+ *    PUT: update user by id
+ *    DELETE: deletes user by id
+ */
+
+app.get("/api/users/:id", userController.getUser);
+app.put("/api/users/:id", userController.updateUser);
+app.delete("/api/users/:id", userController.deleteUser);
+
+
+module.exports = router;
