a3b1298b7d2beb0c00b756bfa1afc57137dc15b6
Alessandro Capra <alessandro.cpr@gmail.com>
2018-05-18 15:41:03 +0200

    Changed location of socket for pressure

--- public/js/main.js
+++ public/js/main.js
@@ -17,10 +17,6 @@ socket.on('value', function(data){
   sensorValue = data.message;
 });
 
-socket.on('pressure', function(data){
-  pressureText.setText('Pressure: ' + data.pressure + 'Pa');
-});
-
 var canvasWidth = window.innerWidth * window.devicePixelRatio;
 var canvasHeight = window.innerHeight * window.devicePixelRatio;
 
@@ -175,6 +171,10 @@ function update ()
     player.setVelocityY(-330);
     // sensorValue = "";
   }
+
+  socket.on('pressure', function(data){
+    pressureText.setText('Pressure: ' + data.pressure + 'Pa');
+  });
 }
 
 // this function is fired when we connect
