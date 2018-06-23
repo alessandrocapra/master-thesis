5f995034bc2ac8a6870f717f52330370783e7cef
Alessandro Capra <alessandro.cpr@gmail.com>
2018-06-23 15:56:46 +0200

    Added error message on wrong login info

--- app/states/login.js
+++ app/states/login.js
@@ -21,6 +21,8 @@ module.exports = {
       placeHolder: 'Name'
     });
 
+		// this.nameInput.startFocus();
+
 		this.passwordInput = this.add.inputField(this.world.centerX - 140, this.world.centerY + 25, {
 			font: '20px Arial',
 			width: 250,
@@ -31,6 +33,11 @@ module.exports = {
 			placeHolder: 'Password',
 			type: PhaserInput.InputType.password
 		});
+
+		// Error message to display if no matching users were found after login
+		var errorMessage = this.add.text(this.world.centerX, this.world.height * 0.9, 'Name and/or password not correct, try again!', {fill: "#ff3647"});
+		errorMessage.anchor.set(0.5);
+		errorMessage.visible = false;
 
 		// login button and text
 		var checkLoginBtn = this.checkLoginBtn= this.add.sprite(this.world.centerX, this.world.centerY+140, 'button', 'blue_button04.png');
@@ -49,22 +56,31 @@ module.exports = {
 
 			console.log('name: ' + name + ', pass: ' + password);
 
+			// connect to API to retrieve all users and check it the data in the form matches
 			var xhr  = new XMLHttpRequest();
 			xhr.open('GET', 'https://duchennegame.herokuapp.com/api/users', true);
 			xhr.onload = function () {
 				var users = JSON.parse(xhr.responseText);
 				if (xhr.readyState == 4 && xhr.status == "200") {
-					console.log(users);
 					var userFound = false;
 
 					users.forEach(function(user){
 						if(user.name === name && user.password === password){
 							self.state.start('game');
-							// userFound = true;
-						} else {
-							console.log("User not found!");
+							userFound = true;
 						}
 					});
+
+					// if user has not been found / wrong password, display error message
+					if(!userFound){
+						errorMessage.visible = true;
+
+						// make text become not visible again after few seconds
+						self.time.events.add(Phaser.Timer.SECOND * 3, function () {
+							errorMessage.visible = false;
+						}, this);
+
+					}
 				} else {
 					console.error(users);
 				}
