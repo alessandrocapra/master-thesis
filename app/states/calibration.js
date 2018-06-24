e754de4818d2426f19f59a91a10855ae5db53229
Alessandro Capra <alessandro.cpr@gmail.com>
2018-06-24 16:56:16 +0200

    using visibility to show or hide circle - 100ms

--- app/states/calibration.js
+++ app/states/calibration.js
@@ -23,14 +23,9 @@ module.exports = {
 		var title = this.add.text(this.game.global.titlePlacement.x, this.game.global.titlePlacement.y, 'Calibration', this.game.global.titleStyle);
 		title.anchor.set(0.5);
 
-		// circle that changes in shape, according to breathing
-		// var largerCircle = this.largerCircle = this.add.graphics(0, 0);
-		// largerCircle.beginFill(this.game.global.primaryColorTint, 0.5);
-		// largerCircle.drawCircle(this.world.centerX, this.world.centerY + 100, 150);
-		var largerCircle = this.largerCircle = new Phaser.Circle(this.world.centerX, this.world.centerY + 100, 150);
-		var graphics = this.graphics = this.add.graphics(0, 0);
-		// largerCircle.beginFill(this.game.global.primaryColorTint, 0.5);
-		// largerCircle.drawCircle(this.world.centerX, this.world.centerY + 100, 150);
+		// draw an empty circle that is going to chane with pressure data
+		this.largerCircle = new Phaser.Circle(this.world.centerX, this.world.centerY + 100, 150);
+		this.graphics = this.add.graphics(0, 0);
 
 		// basic circle, which stays in the middle
 		var middleCircle = this.middleCircle = this.add.graphics(0, 0);
@@ -62,12 +57,13 @@ module.exports = {
 
 		this.largerCircle.diameter = this.updatedCircleDiameter;
 
-		// draw update figure on it
+		// draw update figure on the invisible circle
+		this.graphics.visible = true;
 		this.graphics.beginFill(this.game.global.primaryColorTint, 0.5);
 		this.graphics.drawCircle(this.world.centerX, this.world.centerY + 100, this.largerCircle.diameter);
-		this.graphics.visible = true;
+
 		setTimeout(function(){
 			self.graphics.visible = false;
-		},10);
+		},100);
 	}
 };
