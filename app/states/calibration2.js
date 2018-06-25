13e74ec2360a988ca998dbc60aed603687b6806b
Alessandro Capra <alessandro.cpr@gmail.com>
2018-06-25 11:16:00 +0200

    removing timeout to clear graphics - fixed build

--- app/states/calibration2.js
+++ app/states/calibration2.js
@@ -42,12 +42,44 @@ module.exports = {
 		// draw update figure on the invisible circle
 		this.graphics.clear();
 
-		this.graphics.visible = true;
+		// this.graphics.visible = true;
 		this.graphics.beginFill(this.game.global.primaryColorTint, 0.5);
 		this.graphics.drawCircle(this.world.centerX, this.world.centerY + 100, this.largerCircle.diameter);
 
-		setTimeout(function(){
-			self.graphics.visible = false;
-		},100);
-	}
+		var sprite = this.add.sprite(this.world.centerX, this.world.centerY + 100, this.graphics.generateTexture());
+		sprite.anchor.set(0.5);
+
+		// setTimeout(function(){
+		// 	self.graphics.visible = false;
+		// },100);
+	},
+
+	// tweenTint: function(obj, startColor, endColor, time = 250, delay = 0, callback = null) {
+	// 	// check if is valid object
+	// 	if (obj) {
+	// 		// create a step object
+	// 		let colorBlend = {
+	// 			step: 0
+	// 		};
+	//
+	// 		// create a tween to increment that step from 0 to 100.
+	// 		let colorTween = this.game.add.tween(colorBlend).to({ step: 100 }, time, Phaser.Easing.Linear.None, delay);
+	//
+	// 		// add an anonomous function with lexical scope to change the tint, calling Phaser.Colour.interpolateColor
+	// 		colorTween.onUpdateCallback(() => {
+	// 			obj.tint = Phaser.Color.interpolateColor(startColor, endColor, 100, colorBlend.step);
+	// 		});
+	//
+	// 		// set object to the starting colour
+	// 		obj.tint = startColor;
+	//
+	// 		// if you passed a callback, add it to the tween on complete
+	// 		if (callback) {
+	// 			colorTween.onComplete.add(callback, this);
+	// 		}
+	//
+	// 		// finally, start the tween
+	// 		colorTween.start();
+	// 	}
+	// }
 };
