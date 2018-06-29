650ad4407fbe48c5e698c7dddd56ce569d25fe49
Alessandro Capra <alessandro.cpr@gmail.com>
2018-06-29 10:38:13 +0200

    Socketio for production in calibration

--- app/states/calibration2.js
+++ app/states/calibration2.js
@@ -20,10 +20,10 @@ module.exports = {
 		* */
 
 		// production
-		// this.socket = io.connect(window.location.hostname, { secure: true, reconnect: true, rejectUnauthorized : false } );
+		this.socket = io.connect(window.location.hostname, { secure: true, reconnect: true, rejectUnauthorized : false } );
 
 		//development
-		this.socket = io.connect('http://localhost:5000');
+		// this.socket = io.connect('http://localhost:5000');
 
 		this.socket.on("connect", function () {
 			console.log("client (game) connected to server");
